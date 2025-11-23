0aa53620bb0aafac1acd3d6cf978fd01c0d96634
YangPeng-CNCB <yangpeng@big.ac.cn>
2025-11-23 17:04:02 +0800

    Add files via upload

---
+++ Figure4c.R
@@ -0,0 +1,65 @@
+
+
+#################画图a
+rm(list=ls())
+library(data.table)
+
+aa=fread("coloc/用于画图的数据/画图的数据.txt")
+
+aa$disconsistence=1-aa$consistence
+
+library(tidyr)
+
+# 转换数据为长格式
+data_long <- aa %>%
+  pivot_longer(cols = c(consistence, disconsistence), 
+               names_to = "type", 
+               values_to = "value")
+
+data_long$value=round(data_long$value*100, 1)
+
+
+data_long$category=""
+# 通过条件标识区分meQTL与pQTL
+data_long$category[1:4] <- "meQTL"
+data_long$category[5:8] <- "pQTL"
+
+library(ggplot2)
+
+
+p=ggplot(data_long, aes(x = Dataset, y = value, fill = type)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+  theme_minimal() +
+  labs(x = "", y = "Percentage (%)", fill = "Type") +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size = 14, color = "black"),
+        axis.text.y = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.position = "none",
+        panel.grid.major = element_blank(),      # 去除主网格线
+        panel.grid.minor = element_blank(),      # 去除次网格线
+        axis.line = element_line(color = "black")) +  # 添加x轴和y轴
+  scale_x_discrete(labels = function(x) gsub("\\(", "\n(", x),
+                   limits = unique(data_long$Dataset)) +
+  scale_fill_manual(values = c("consistence" = "#fc8d62", "disconsistence" = "#a6d854")) +
+  geom_text(aes(label = value), position = position_dodge(width = 0.9), vjust = -0.5, size = 5)
+p
+ggsave("coloc/用于画图的数据/fig4c.png", p,
+       width = 8, height = 5.5, dpi = 300, bg = "white")  # 调整图像的宽度和高度
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
